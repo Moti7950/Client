30e2b6edf20de46eeb6fabe8cb7555ae2c3e531c
Moti7950 <shalom7950@gmail.com>
2025-09-07 11:49:45 +0300

    part order files finish

---
+++ src/components/post.component.tsx
@@ -0,0 +1,15 @@
+export default function Post()
+{
+    return(
+        <>
+        <h1>Welcom from post</h1>
+        <div id="post-controler">
+        <img id="img-post" src="" alt="" />
+        <h3 id="explen-post"></h3>
+        <div id="likes-post"></div>
+        <div id="ownerName-post"></div>
+        <div id="dataAndTime-post"></div>
+        </div>
+        </>
+    )
+}
---
+++ src/page/home.page.tsx
@@ -0,0 +1,11 @@
+import Post from "../components/post.component.tsx"
+
+export default function HomePage()
+{
+    return(
+        <>
+        <h1>Welcome from home page</h1>
+        <Post/>
+        </>
+    )
+}
